fa3b3dfd0fb76844c10b277a11126f941bf28e14
JasonXBlue <JasonX.blue1@gmail.com>
2020-01-28 22:21:56 -0600

    1/28/20

---
+++ CS321_W3D1_BookAPI/CS321_W3D1_BookAPI/Controllers/BooksController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CS321_W3D1_BookAPI.Models;
+using CS321_W3D1_BookAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CS321_W3D1_BookAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBookService _bookService;
+
+        public BooksController(IBookService bookService)
+        {
+            _bookService = bookService;
+        }
+        // GET api/books
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_bookService.GetAll());
+        }
+
+        // GET api/books/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int Id)
+        {
+            // look up book by id
+            var book = _bookService.Get(Id);
+            // if not found, return 404 NotFound 
+            if (book == null) return NotFound();
+            // otherwise return 200 OK and the Book
+            return Ok(book);
+        }
+
+        // POST api/books
+        [HttpPost]
+        public IActionResult Post([FromBody] Book newBook)
+        {
+            try
+            {
+                // add the new book
+                _bookService.Post(newBook);
+            }
+            catch (System.Exception ex)
+            {
+                ModelState.AddModelError("Add new book", ex.Message);
+                return BadRequest(ModelState);
+            }
+
+            // return a 201 Created status. This will also add a "location" header
+            // with the URI of the new book. E.g., /api/books/99, if the new is 99
+            return CreatedAtAction("Get", new { Id = newBook.Id }, newBook);
+
+        }
+
+        // PUT api/books/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Book updatedBook)
+        {
+            var book = _bookService.Update(updatedBook);
+            if (book == null) return NotFound();
+            return Ok(book);
+        }
+
+        // DELETE api/books/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int Id)
+        {
+            var book = _bookService.Get(Id);
+            if (book == null) return NotFound();
+            _bookService.Remove(book);
+            return NoContent();
+
+        }
+    }
+}
---
+++ CS321_W3D1_BookAPI/CS321_W3D1_BookAPI/Services/BookService.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CS321_W3D1_BookAPI.Data;
+using CS321_W3D1_BookAPI.Models;
+
+namespace CS321_W3D1_BookAPI.Services
+{
+    public class BookService : IBookService
+    {
+        private readonly BookContext _bookContext;
+
+        public BookService(BookContext bookContext)
+        {
+            _bookContext = bookContext;
+        }
+
+        public void Remove(Book deletedBook)
+        {
+            _bookContext.Books.Remove(deletedBook);
+            _bookContext.SaveChanges();
+           
+        }
+
+        public Book Get(int bookId)
+        {
+            return _bookContext.Books.FirstOrDefault(b => b.Id == bookId);
+        }
+
+        public IEnumerable<Book> GetAll()
+        {
+            return _bookContext.Books.ToList();
+        }
+
+        public Book Post(Book newBook)
+        {
+            // store in the list of books
+            _bookContext.Books.Add(newBook);
+            _bookContext.SaveChanges();
+            // return the new book with Id filled in
+            return newBook;
+        }
+
+        public Book Update(Book updatedBook)
+        {
+            // get the book object in the current list with this id 
+            var currentBook = _bookContext.Books.Find(updatedBook.Id);    
+
+            // return null if book to update isn't found
+            if (currentBook == null) return null;
+
+            // copy the property values from the changed book into the
+            // one in the db. NOTE that this is much simpler than individually
+            // copying each property.
+            _bookContext.Entry(currentBook)
+                .CurrentValues
+                .SetValues(updatedBook);
+
+            // update the book and save
+            _bookContext.Books.Update(currentBook);
+            _bookContext.SaveChanges();
+            return currentBook;
+        }
+    }
+}
